8a58eca700f4f18c711f0fbef78569ffc389e917
Ivanchenko Dmitry <ivanchenko.dimitry@gmail.com>
2011-07-19 19:05:36 +0300

    Events 0.2.3

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -8,6 +8,6 @@ urlpatterns = patterns('',
     # Add event
     url(r'^add_event/$', 'events.views.add_event', name="add_event"),
     # an event details
-    url(r'^details/(?P<id>\d+)/$', 'events.views.details', name="events_details"),
-    url(r'^destroy/(?P<id>\d+)/$', 'events.views.destroy', name='events_destroy'),
+    url(r'^details/(?P<id>\d+)/$', 'events.views.details', name="event_details"),
+    url(r'^destroy/(?P<id>\d+)/$', 'events.views.destroy', name='event_destroy'),
 )
--- apps/events/views.py
+++ apps/events/views.py
@@ -34,8 +34,6 @@ def destroy(request, id):
 @login_required
 def details(request, id, template_name="events/details.html"):
 
-	redirect_to = '/events/'
-    
 	event = Event.objects.get(id = id)
 
 	user = request.user.username
@@ -67,8 +65,10 @@ def add_event(request, template_name="events/add_event.html"):
 			new_event.address = request.POST.get("address")
 			new_event.creator = request.user.username
 			new_event.save()
-		
-        	return render_to_response('events/latest.html', context_instance=RequestContext(request))
+			
+			include_kwargs = {"id": new_event.id}
+			redirect_to = reverse("event_details", kwargs=include_kwargs)
+			return HttpResponseRedirect(redirect_to)
     return render_to_response(template_name, context_instance=RequestContext(request))
 
 
