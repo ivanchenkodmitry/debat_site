9d198de1100dfe0977e86be829dd803892812448
Ivanchenko Dmitry <ivanchenko.dimitry@gmail.com>
2011-08-04 14:07:14 +0300

    2 widget on home

--- views.py
+++ views.py
@@ -13,23 +13,21 @@ from django.core.paginator import Paginator
 from django.contrib.auth.models import User
 from blog.models import Post
 
+from events.models import Event
+
 def homepage_view (request, template_name = "homepage.html"):
         
 	posts = Post.objects.filter(status2=1).order_by("-publish")
         paginator = Paginator(posts, 3)
-
-        
-       
-
-	
-		
 		
         adminposts = Post.objects.filter(status2=1).order_by("-publish")
         adminpaginator = Paginator(adminposts, 3)
 
         
-       
+	events = Event.objects.order_by("title")       
 
 	return render_to_response(template_name, {
-		"adminposts": adminposts, 'posts':posts,
+		"adminposts": adminposts,
+		'posts':posts,
+		'events': events,
 		}, context_instance=RequestContext(request))
