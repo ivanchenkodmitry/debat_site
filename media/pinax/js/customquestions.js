da565ef8ef0f2e507baebb0155d50626d81fcf6a
Constantine Fedenko <cfedenko@gmail.com>
2011-08-18 20:30:29 +0300

    Custom questions integration

--- media/pinax/js/customquestions.js
+++ media/pinax/js/customquestions.js
@@ -1,172 +1,270 @@
 //Custom qustion editor
 
-(function($) {
-    qestionnum = 0;
-    answernum = 0;
-    
-    $( "#questioncontainer" ).sortable();
-    //$( "#questioncontainer" ).disableSelection();
+(function( $ ){
 
-    
-    $( "#questioncontainer" ).bind( "sortstop", updateTitles)
-    
-    function updateTitles(event) {
-        var questions = $('#questioncontainer').sortable('toArray');
-        count = questions.length;
-        for (i=0;i<=count;i++) {
-            $('#' + questions[i]).find('label[for="id_' + questions[i] + '_title"]').text('Питання '+ (i+1))
-        }
-    }
-    
-    $("#addquestion").click(addQestion);
-//    
-//    $("#submit").click({msg: 'test title'}, onSubmit);
-//    
-//    function onSubmit(event) {
-//        $('#id_title').val(event.data.msg);
-//        window.alert(event.data.msg);
-//    }
-    
-    function addQestion(){
-        var count = $("#questioncontainer").children().length;
-        var num = count + 1;
-        qestionnum = qestionnum + 1;
-        var id = qestionnum;
-        if (count < 10) { 
-            $question = $('<li>');
-            $question.attr('id', 'q' + id);
-            $question.append('\
-            <div class="ctrlHolder">\
-              <label class="qtitle" for="id_q' + id +'_title">\
-                Питання '+ num +'\
-              </label>\
-              <textarea id="id_q' + id +'_title" class="textarea" name="q' + id +'_title" cols="40" rows="10"></textarea>\
-            </div>');
-            $question.append('\
-            <div class="ctrlHolder">\
-              <label for="id_q' + id +'_type">\
-                Тип відповіді\
-              </label>\
-              <select id="id_q' + id +'_type" name="q' + id +'_type" class="select">\
-                <option selected="selected" value="1">Вільна відповіль</option>\
-                <option value="2">Вибір однієї правильної</option>\
-                <option value="3">Вибір кількох правильних</option>\
-              </select></div>');
-            $question.append('\
-            <ul id="id_q' + id +'_answercontainer" class="answercontainer"></ul>');
-            $question.append('\
-            <div class="ctrlHolder">\
-              <input id="id_q' + id +'_addanswer" type="button" value="Додати відповідь">\
-              <input id="id_q' + id +'_rm" type="button" style="float:right;" value="Видалити">\
-            </div>');
-            //$question.append('<div class="form_block"><input id="id_q' + id +'_rm" type="button" value="Видалити"></div>');
-                        
-            $('#questioncontainer').append($question);
-            
-            $answercontainer = $('#id_q' + id +'_answercontainer');
-            
-            $('#id_q' + id +'_addanswer').hide();
-            $answercontainer.hide();
-            
-            $('#id_q' + id +'_type').change({id: id}, onTypeChange);
-            $('#id_q' + id +'_rm').click({id: id}, removeQestion);
-            
-            $answercontainer.sortable();
-            $answercontainer.bind( "sortstop", {id: id}, updateAnswerTitles)
-            $('#id_q' + id +'_addanswer').click({id: id}, addAnswer);
-            
-            $('#id_q' + id +'_addanswer').click();
-            $('#id_q' + id +'_addanswer').click();
-            
-            var firstanswerid = $answercontainer.find('li').first().attr('id');
-            $answercontainer.find('input:radio, input:checkbox').val([firstanswerid]);
-        }
-    }
-    
-    function removeQestion(event){
-        $('#q' + event.data.id).remove();
-        updateTitles(event);
-    }
-    
-    function updateAnswerTitles(event) {
-        var answers = $('#id_q' + event.data.id +'_answercontainer').sortable('toArray');
-        count = answers.length;
-        for (i=0;i<=count;i++) {
-            $('#' + answers[i]).find('label[for="id_' + answers[i] + '_title"]').text('Відповідь '+ (i+1))
-        }
+  $.fn.testeditor = function() {
         
+    this.find(':submit').remove();
+    
+    this.after('\
+            <h2>Додаткові питання</h2>\
+            <form class="uniForm">\
+              <fieldset class="inlineLabels">\
+                <ul id="itemcontainer">\
+                </ul>\
+                <div class="form_block">\
+                    <input id="additem" type="button" value="Додати питання">\
+                </div>\
+                <div class="form_block">\
+                    <input id="psevdosubmit" type="button" value="Надіслати">\
+                </div>\
+              </fieldset>\
+            </form>');
+    
+    var itemnum = 0,
+        optionnum = 0;
+        
+    $("#itemcontainer").sortable();
+    //$( "#itemcontainer" ).disableSelection();
+            
+    $("#additem").click(onAddItem);
+    
+    $("#itemcontainer" ).bind( "sortstop", onUpdateTitles)
+    
+    this.submit(onSubmit); 
+               
+    $('#psevdosubmit').click(function() {
+    $("#edit-profile").submit();
+    });
+    
+    var data = $('#id_questions').val();
+    
+    if (data != '')  {       
+        var data = $.secureEvalJSON(data);
+        for (i=0;i<data.length;i++) {
+            addItem(data[i]);
+        }
     }
-     
-    function addAnswer(event){
-        $answercontainer = $('#id_q' + event.data.id +'_answercontainer');
-        var type = $('#id_q' + event.data.id +'_type').val();
-        var count = $answercontainer.children().length;
+        
+    function onAddItem(event){
+        addItem();
+    }
+    
+    function addItem(data){
+        var count = $("#itemcontainer").children().length;
         var num = count + 1;
-        answernum = answernum +1;
-        var id = answernum;
+        itemnum = itemnum + 1;
+        var id = itemnum;
+        if (count < 10) { 
+            $item = $('\
+                <li id="i'+id +'">\
+                  <div class="ctrlHolder">\
+                    <label class="itemtitle" for="id_i' + id +'_title">\
+                      Питання '+ num +'\
+                    </label>\
+                    <textarea id="id_i' + id +'_title" class="textarea" cols="40" rows="10"></textarea>\
+                  </div>\
+                  <div class="ctrlHolder">\
+                    <label for="id_i' + id +'_type">Тип відповіді</label>\
+                    <select id="id_i' + id +'_type" class="select">\
+                      <option selected="selected" value="1">Вільна відповіль</option>\
+                      <option value="2">Вибір однієї правильної</option>\
+                      <option value="3">Вибір кількох правильних</option>\
+                    </select>\
+                  </div>\
+                  <ul id="id_i' + id +'_optioncontainer" class="optioncontainer"></ul>\
+                  <div class="ctrlHolder">\
+                    <input id="id_i' + id +'_addoption" type="button" value="Додати відповідь">\
+                    <input id="id_i' + id +'_rm" type="button" style="float:right;" value="Видалити">\
+                  </div>\
+                </li>');
+                
+            //TODO type names
+                        
+            $('#itemcontainer').append($item);
+            
+            $optioncontainer = $('#id_i' + id +'_optioncontainer');
+            
+            $('#id_i' + id +'_addoption').hide();
+            $optioncontainer.hide();
+            
+            $('#id_i' + id +'_type').change({id: id}, onTypeChange);
+            $('#id_i' + id +'_rm').click({id: id}, removeItem);
+            
+            $optioncontainer.sortable();
+            $optioncontainer.bind( "sortstop", {itemid: id}, updateOptionTitles)
+            $('#id_i' + id +'_addoption').click({id: id}, onAddOption);
+            
+            if (data != undefined) {
+                $('#id_i' + id +'_title').val(data.title)
+                $('#id_i' + id +'_type').val(data.type)
+                $('#id_i' + id +'_type').change()
+                console.log(data.type);
+                if (data.type != '1') {
+                    console.log(data.options);
+                    for (i=0;i<data.options.length;i++) {
+                        addOption(id,data.options[i]);
+                    }
+                    return true;
+                }
+            }
+            
+            addOption(id, {right: true});
+            addOption(id);
+            
+        }
+    }
+    
+    function removeItem(event){
+        $('#i' + event.data.id).remove();
+        updateTitles();
+    }
+    
+    function onUpdateTitles(event) {
+        updateTitles();
+    }
+    
+    function updateTitles() {
+        $('#itemcontainer > li').each(function(i) {
+            //TODO
+            $(this).find('label.itemtitle').text('Питання '+ (i+1))
+        });
+    }
+    
+    function onAddOption(event){        
+        addOption(event.data.id);
+    }
+    
+    function addOption(itemid, data){
+        $optioncontainer = $('#id_i' + itemid +'_optioncontainer');
+        var type = $('#id_i' + itemid +'_type').val();
+        var count = $optioncontainer.children().length;
+        var num = count + 1;
+        optionnum = optionnum +1;
+        var id = optionnum;
         if (count < 10) {        
-            $ctrlholder = $('<div class="ctrlHolder">');
-            $ctrlholder.append('<label for="id_a' + id +'_title"> Відповідь ' + num +'</label>');
-            $ctrlholder.append('<input id="id_a' + id +'_title" class="textinput" type="text" maxlength="200" name="a' + id +'_title">');
-            $ctrlholder.append('<input id="id_a' + id +'_rm" type="button" value="Видалити"><br>');
-            $ctrlholder.append('<label for="id_a' + id +'_right">Правильна</label>');
-            $ctrlholder.append('<input id="id_a' + id +'_rbright" class="radioboxinput" type="radio" name="q' + event.data.id +'_rbright" value="a' + id + '">');
-            $ctrlholder.append('<input id="id_a' + id +'_cbright" class="checkboxinput" type="checkbox" name="a' + id +'_rbright" value="a' + id + '">');
-            $answer = $('<li>');
-            $answer.attr('id', 'a' + id);
-            $answer.append($ctrlholder);
-            $answercontainer.append($answer);
+            $option = $('\
+                <li id="o'+ id +'">\
+                  <div class="ctrlHolder">\
+                    <label for="id_o' + id +'_title"> Відповідь ' + num +'</label>\
+                    <input id="id_o' + id +'_title" class="textinput" type="text" maxlength="200">\
+                    <input id="id_o' + id +'_rm" type="button" value="Видалити"><br>\
+                    <label for="id_o' + id +'_right">Правильна</label>\
+                    <input id="id_o' + id +'_rbright" class="radioboxinput" type="radio"\
+                           name="i' + itemid +'_rbright" value="o' + id + '">\
+                    <input id="id_o' + id +'_cbright" class="checkboxinput" type="checkbox"\
+                           name="o' + id +'_rbright" value="o' + id + '">\
+                  </div>\
+                </li>');
+
+            $optioncontainer.append($option);
+            
+            if (data != undefined) {
+
+            if (data.title != undefined) {
+                $('#id_o' + id +'_title').val(data.title);
+            }
+            
+            //XXX 1 first radio checked only
+            if (data.right != undefined) {
+                $('#id_o' + id +'_cbright').attr('checked', data.right);
+                $('#id_o' + id +'_rbright').attr('checked', data.right);
+            }
+            
+            }
             
             switch(type)
             {
             case '2': //Вибір однієї правильної
-              $answer.find('input:checkbox').hide()
-              $answer.find('input:radio').show()
+              $option.find('input:checkbox').hide()
+              $option.find('input:radio').show()
               break;
             case '3': //Вибір кількох правильних
-              $answer.find('input:checkbox').show()
-              $answer.find('input:radio').hide()
+              $option.find('input:checkbox').show()
+              $option.find('input:radio').hide()
               break;
             }
         
-            $('#id_a' + id +'_rm').click({id: event.data.id, aid: id}, removeAnswer);
+            $('#id_o' + id +'_rm').click({itemid: itemid, optionid: id}, removeOption);
         }
-        //TODO add check handler
+        //TODO 2 add check handler (see 4)
     }
     
-    function removeAnswer(event){
-        var count = $('#id_q' + event.data.id +'_answercontainer').children().length;
+    function removeOption(event){
+        var count = $('#id_i' + event.data.itemid +'_optioncontainer').children().length;
         if (count>2) {        
-            $('#a' + event.data.aid ).remove()
+            $('#o' + event.data.optionid ).remove()
         }
-        updateAnswerTitles(event);
-        //TODO do not remove checked answer
-    }  
+        updateOptionTitles(event);
+        //TODO 3 do not remove checked option (see 4)
+    }   
+    
+    function updateOptionTitles(event) {
+        $('#id_i' + event.data.itemid +'_optioncontainer > li').each(function(i) {
+            $(this).find('label[for$="_title"]').text('Відповідь '+ (i+1));
+        });
+    }
     
     function onTypeChange(event){
-        var type = $('#id_q' + event.data.id +'_type').val();
-        $answercontainer = $('#id_q' + event.data.id +'_answercontainer');
+        var type = $('#id_i' + event.data.id +'_type').val();
+        $optioncontainer = $('#id_i' + event.data.id +'_optioncontainer');
                 
         switch(type)
         {
         case '1': //Вільна відповіль
-          $('#id_q' + event.data.id +'_addanswer').hide();
-          $answercontainer.hide();
+          $('#id_i' + event.data.id +'_addoption').hide();
+          $optioncontainer.hide();
           break;
         case '2': //Вибір однієї правильної
-          $('#id_q' + event.data.id +'_addanswer').show();
-          $answercontainer.show();
-          $answercontainer.find('input:checkbox').hide()
-          $answercontainer.find('input:radio').show()
+          $('#id_i' + event.data.id +'_addoption').show();
+          $optioncontainer.show();
+          $optioncontainer.find('input:checkbox').hide()
+          $optioncontainer.find('input:radio').show()
           break;
         case '3': //Вибір кількох правильних
-          $('#id_q' + event.data.id +'_addanswer').show();
-          $answercontainer.show();
-          $answercontainer.find('input:checkbox').show()
-          $answercontainer.find('input:radio').hide()
+          $('#id_i' + event.data.id +'_addoption').show();
+          $optioncontainer.show();
+          $optioncontainer.find('input:checkbox').show()
+          $optioncontainer.find('input:radio').hide()
           break;
         }
     }
+    
+    function getData(){
+        var items = $('#itemcontainer').sortable('toArray');
+        
+        var types = {
+            '2':'radio',
+            '3':'checkbox'        
+        }
+        
+        var data = new Array();    
+        
+        $('#itemcontainer > li').each(function(i) {
+            var item = new Object();
+            item.title = $(this).find('textarea').val();
+            item.type = $(this).find('select').val();
+            if (item.type != '1') {
+                item.options = new Array();
+                $(this).find('li').each(function(j) {
+                    var option = new Object();
+                    option.title = $(this).find('input:text').val();
+                    if ($(this).find('input:'+ types[item.type]).attr('checked')) {
+                        option.right = true;
+                    }
+                    item.options.push(option);
+                });
+            }            
+            data.push(item);
+        });
+        return data;
+    }
+    
+    function onSubmit(event) {
+        $('#id_questions').val($.toJSON(getData()));
+        return true;
+        //TODO 4 add some validation
+    }
 
-
-})(jQuery);
+  };
+})( jQuery );
