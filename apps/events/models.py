fa08e27278882314cb5674d995b684d2780a10f8
Niko Skrypnik <nskrypnik@gmail.com>
2011-08-24 13:06:08 +0300

    Fix member and events relationship

--- apps/events/models.py
+++ apps/events/models.py
@@ -17,7 +17,6 @@ class Member(models.Model):
 	def __unicode__(self):
 		return self.user.get_profile().surname + ' ' + self.user.get_profile().name
 
-
 class Event(models.Model):
     """
     An event with its details
@@ -28,7 +27,7 @@ class Event(models.Model):
     address = models.TextField(_('addres'), blank=True)
     date_added = models.DateTimeField(_('date added'), default=datetime.now, editable=False)
     creator = models.ForeignKey(User)
-    members = models.ManyToManyField(Member, verbose_name="members_list", blank=True)
+    members = models.ForeignKey(Member, verbose_name="members_list", blank=True)
     location = models.CharField(_('location'), max_length=200)
     questions = models.TextField(blank=True)
 	
