8f75dce4b800bb6ac637190c17c4e8194a70d52d
Ivanchenko Dmitry <ivanchenko.dimitry@gmail.com>
2011-09-08 21:40:41 +0300

    Profile&clubs

--- apps/profiles/admin.py
+++ apps/profiles/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from profiles.models import Profile
+from profiles.models import Profile, Verification
 
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'surname', 'club')
-    list_filter         = ('user', 'surname', 'education', 'club')
+    list_display = ('user', 'surname')
+    list_filter         = ('user', 'surname', 'education')
+
+class VerificationAdmin(admin.ModelAdmin):
+    list_display = ('profile', 'md5_hash')
+    
 admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Verification, VerificationAdmin)
