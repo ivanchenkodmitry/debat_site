d78605b2af4c72594c947b823637fd186957a25b
Bogdan Marchenko <bogdan.marko@gmail.com>
2011-10-17 14:13:00 +0200

    lightbox and photos final

--- media/js/lightbox/lightbox.js
+++ media/js/lightbox/lightbox.js
@@ -1,53 +1,12 @@
-// -----------------------------------------------------------------------------------
-//
-//	Lightbox v2.05
-//	by Lokesh Dhakar - http://www.lokeshdhakar.com
-//	Last Modification: 3/18/11
-//
-//	For more information, visit:
-//	http://lokeshdhakar.com/projects/lightbox2/
-//
-//	Licensed under the Creative Commons Attribution 2.5 License - http://creativecommons.org/licenses/by/2.5/
-//  	- Free for use in both personal and commercial projects
-//		- Attribution requires leaving author name, author link, and the license info intact.
-//	
-//  Thanks: Scott Upton(uptonic.com), Peter-Paul Koch(quirksmode.com), and Thomas Fuchs(mir.aculo.us) for ideas, libs, and snippets.
-//  		Artemy Tregubenko (arty.name) for cleanup and help in updating to latest ver of proto-aculous.
-//
-// -----------------------------------------------------------------------------------
-/*
 
-    Table of Contents
-    -----------------
-    Configuration
-
-    Lightbox Class Declaration
-    - initialize()
-    - updateImageList()
-    - start()
-    - changeImage()
-    - resizeImageContainer()
-    - showImage()
-    - updateDetails()
-    - updateNav()
-    - enableKeyboardNav()
-    - disableKeyboardNav()
-    - keyboardAction()
-    - preloadNeighborImages()
-    - end()
-    
-    Function Calls
-    - document.observe()
-   
-*/
 // -----------------------------------------------------------------------------------
 
 //
 //  Configurationl
 //
 LightboxOptions = Object.extend({
-    fileLoadingImage:        'images/loading.gif',     
-    fileBottomNavCloseImage: 'images/closelabel.gif',
+    fileLoadingImage:        '/site_media/static/css/images/loading.gif',     
+    fileBottomNavCloseImage: '/site_media/static/css/images/closelabel.gif',
 
     overlayOpacity: 0.8,   // controls transparency of shadow overlay
 
