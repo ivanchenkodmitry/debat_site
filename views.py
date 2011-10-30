4e1fc979b8803b50fbb25fb1d626debc9cd19dd6
Constantine Fedenko <cfedenko@gmail.com>
2011-10-30 16:24:01 +0200

    Nearest event

--- views.py
+++ views.py
@@ -7,23 +7,29 @@ from blog.models import Post
 
 from events.models import Event
 from photos.models import Image, Pool
-import random
+#import random
+from datetime import datetime
 
 def homepage_view (request, template_name = "homepage.html"):
         
     adminposts = Post.objects.filter(author__is_staff=True, status2=1).order_by("-publish")
     posts = Post.objects.filter(author__is_staff=False, status2=1).order_by("-publish")
         
-    events = Event.objects.filter(approved=True).order_by("title")
-
+    events = Event.objects.filter(approved=True).order_by("date")
+    nearest_events = events.filter(date__gte=datetime.now())
+    if nearest_events:
+        _nearest_event = nearest_events[0]
+        nearest_event = list(events).index(_nearest_event)
+    else:
+        nearest_event = None
+    
     rand_photos = Image.objects.order_by('?')
         
-
-
     return render_to_response(template_name, {
-		"adminposts": adminposts,
-		'posts':posts,
-		'events': events,
-                'rand_photo': rand_photos,
-		}, context_instance=RequestContext(request))
+        'adminposts': adminposts,
+        'posts':posts,
+        'events': events,
+        'nearest_event': nearest_event,
+        'rand_photo': rand_photos,
+        }, context_instance=RequestContext(request))
 
