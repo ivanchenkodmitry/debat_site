0eab811d993bcdaedde430dc7340a541c5eec800
Bogdan Marchenko <bogdan.marko@gmail.com>
2011-10-31 20:33:45 +0200

    ticket 14 geocode button

--- apps/events/forms.py
+++ apps/events/forms.py
@@ -4,16 +4,29 @@ from events.models import Event, AnswerList
 from django.utils.translation import ugettext_lazy as _
 
 from django.utils import simplejson
+from django.forms.widgets import Textarea
+from django.utils.safestring import mark_safe
+from django.forms.util import flatatt
+from django.utils.html import conditional_escape
+from django.utils.encoding import force_unicode
+
+class EventAddressWidget(Textarea):
+    def render(self, name, value, attrs=None):
+        result = super(EventAddressWidget, self).render(name, value, attrs=attrs)
+        result += mark_safe(u'<input style ="float: right; margin-right: 33px; margin-top: 5px;"  type="button" value="Знайти на мапі" onclick="codeAddress()">')
+        return result
 
 
 class EventForm(forms.ModelForm):
     questions = forms.CharField(widget=forms.HiddenInput, required = False)
     location = forms.CharField(initial="(48.464954, 35.044956)", #dnipropetrovsk
                                widget=forms.HiddenInput)
-    
+    address = forms.CharField(widget=EventAddressWidget())
+
     class Meta:
         model = Event
         exclude = ('members', 'creator', 'approved', 'gallery')
+        
         
     def __init__(self, user=None, *args, **kwargs):
         self.user = user        
