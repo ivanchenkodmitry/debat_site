91a1d7de98f95a0fccf76e1412cf6c6ed83a85f9
Bogdan Marchenko <bogdan.marko@gmail.com>
2011-10-31 13:57:41 +0200

    widget background and event buttons

--- settings.py
+++ settings.py
@@ -38,7 +38,7 @@ TIME_ZONE = 'US/Eastern'
 # Language code for this installation. All choices can be found here:
 # http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
 # http://blogs.law.harvard.edu/tech/stories/storyReader$15
-LANGUAGE_CODE = 'en'
+LANGUAGE_CODE = 'uk'
 
 SITE_ID = 1
 
@@ -270,11 +270,8 @@ INTERNAL_IPS = (
 
 ugettext = lambda s: s
 LANGUAGES = (
-    ('en', u'English'),
-    ('ru', u'Russian'),
-    ('fr', u'France'),
-    ('it', u'Italiano'),
-)
+      ('uk', u'Ukrainian'),
+    )
 
 # URCHIN_ID = "ua-..."
 
