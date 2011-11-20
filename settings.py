4bd96eea00c08aa346262b066426826b81eb60b7
Ivanchenko Dmitry <ivanchenko.dimitry@gmail.com>
2011-11-20 03:12:48 +0200

    data

--- settings.py
+++ settings.py
@@ -6,11 +6,6 @@ import posixpath
 import pinax
 
 
-try:
-   from local_settings import *
-except ImportError:
-   pass
-
 PINAX_ROOT = os.path.abspath(os.path.dirname(pinax.__file__))
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
@@ -33,7 +28,7 @@ ADMINS = (
 # although not all variations may be possible on all operating systems.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'US/Eastern'
+TIME_ZONE = 'Europe/Kiev'
 
 # Language code for this installation. All choices can be found here:
 # http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
